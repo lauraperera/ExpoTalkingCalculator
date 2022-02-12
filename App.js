9ae715fa34f090ceaf545e61ba48b17cbb954974
Laura Perera <lauraperera.pss@gmail.com>
2022-02-11 21:05:42 -0300

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'; //importa tudo do react
+import {Text, TouchableOpacity, TextInput, View, StyleSheet, StatusBar} from 'react-native'; //componentes do react native
+import * as Speech from 'expo-speech';
+import arquivoEstilo from './arquivoEstilo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  state={
+    valor1:0.0,
+    valor2:0.0,
+    resultado:0.0
+  }
+
+  atualizaValor1=(number)=>{
+    this.setState({valor1:number})
+  }
+
+  atualizaValor2=(number)=>{
+    this.setState({valor2:number})
+  }
+  
+  falar(){
+    Speech.speak(this.state.resultado.toString(), {
+      language: "pt-BR"
+    });
+  }
+
+
+  soma(){
+    this.state.resultado = parseFloat(this.state.valor1) + parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+    this.falar();
+  }
+
+  sub(){
+    this.state.resultado = parseFloat(this.state.valor1) - parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+    this.falar();
+  }
+
+  mult(){
+    this.state.resultado = parseFloat(this.state.valor1) * parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+    this.falar();
+  }  
+
+  div(){
+    this.state.resultado = parseFloat(this.state.valor1) / parseFloat(this.state.valor2);
+    alert(this.state.resultado);
+    this.falar();
+  }
+
+  render(){
+    return(
+      <View>
+        <StatusBar backgroundColor="black"/>
+
+        <TextInput style={arquivoEstilo.TextInput}
+          //esse tbm nao
+          placeholder="Valor 1"
+          //esse nao da p tirar
+          onChangeText={this.atualizaValor1}
+        />
+
+        <TextInput style={arquivoEstilo.TextInput}
+          placeholder="Valor 2"
+          onChangeText={this.atualizaValor2}
+        />
+
+        <TouchableOpacity onPress={()=>this.soma(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={arquivoEstilo.submitButton}>Somar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.sub(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={arquivoEstilo.submitButton}>Subtrair</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.mult(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={arquivoEstilo.submitButton}>Multiplicar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>this.div(this.state.valor1, this.state.valor2, this.state.resultado)}>
+          <Text style={arquivoEstilo.submitButton}>Dividir</Text>
+        </TouchableOpacity>
+
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ arquivoEstilo.js
@@ -0,0 +1,26 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+
+    TextInput:{
+        margin:15,
+        height:40,
+        width: 100,
+        borderColor:'#7a42f4',
+        borderWidth:1,
+        borderRadius: 10
+    },
+  
+    submitButton:{
+        backgroundColor:'#7a42f4',
+        padding:10,
+        margin:15,
+        height:40,
+        width: 100,
+        color: '#ffff',
+        alignItems:'center',
+        textAlign:'center',
+        borderRadius: 10
+    },
+
+})
